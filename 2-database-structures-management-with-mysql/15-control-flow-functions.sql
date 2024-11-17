9862721e2f74cb70ff18a7b0b819de2489237f8f
pranto mollick <prantocse01@gmail.com>
2024-11-17 13:01:43 +0600

    control flow functions

--- 2-database-structures-management-with-mysql/15-control-flow-functions.sql
+++ 2-database-structures-management-with-mysql/15-control-flow-functions.sql
@@ -3,3 +3,24 @@
 --------------------------------------------------
 -- Control Flow Functions in MySQL
 --------------------------------------------------
+
+-- 1 Identify common MySQL control flow functions
+-- 2 Explain the benefits of MySQL control flow functions
+
+-- Control flow functions
+    -- Evaluate conditions and determine the execution path of a query
+
+-- CASE
+    -- Runs through a set of conditions contained within a CASE block and returns a value when the first condition is met
+
+SELECT * FROM sales_revenue;
+
+
+SELECT `ItemID`,
+CASE
+    WHEN LEAST(Q1,Q2,Q3,Q4) <= 25000 THEN "Loss"
+    ELSE "Profit"
+END AS Profit_Loss
+FROM sales_revenue;
+
+-- The CASE function is used to evaluate conditions and return a value when the first condition is met.
